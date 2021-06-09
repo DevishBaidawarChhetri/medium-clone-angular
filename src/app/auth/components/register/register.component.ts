408dcfbc94f969963bd954177743ae2f5ac83938
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-06-09 18:26:56 +0545

    creating selector

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
 import { registerAction } from '../../store/actions';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
+
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
+
   constructor (
     private fb: FormBuilder,
     private store: Store
@@ -17,8 +23,16 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void{
     this.initializeForm();
+    this.initializeValues();
   }
-  initializeForm(): void{
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(
+      select(isSubmittingSelector)
+    );
+  };
+
+  initializeForm(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
