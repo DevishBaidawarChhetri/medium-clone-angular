e849762ec68e248a314d6ce9a3064cb5e8588e47
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-06-11 14:20:58 +0545

    Createing feed store (ActionTypes/Action/Effect/Reducer)

---
+++ src/app/shared/modules/feed/store/effects/getFeed.effect.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { of } from "rxjs";
+import { catchError, map, switchMap } from "rxjs/operators";
+
+import { FeedService } from "../../services/feed.service";
+import { GetFeedResponseInterface } from "../../types/getFeedResponse.interface";
+import { getFeedAction, getFeedFailureAction, getFeedSuccessAction } from "../actions/getFeed.action";
+
+@Injectable()
+
+export class GetFeedEffect {
+
+  constructor(
+    private actions$: Actions,
+    private feedService: FeedService,
+  ){}
+
+  getFeed$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getFeedAction),
+      switchMap(({url}) => { // destructuing url so directly => {url}
+        return this.feedService.getFeed(url).pipe(
+          map((feed: GetFeedResponseInterface) => {
+            return getFeedSuccessAction({feed})
+          }),
+          catchError(() => {
+            return of(
+              getFeedFailureAction()
+            )
+          })
+        )
+      })
+    )
+  )
+}
