bcffd0d2da24093223459b55506063f280ec7180
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2021-06-13 17:27:02 +0545

    pagination added

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { select, Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { environment } from "src/environments/environment";
 import { getFeedAction } from "../../store/actions/getFeed.action";
 import { errorSelector, feedSelector, isLoadingSelector } from "../../store/selectors";
 import { GetFeedResponseInterface } from "../../types/getFeedResponse.interface";
@@ -11,29 +13,47 @@ import { GetFeedResponseInterface } from "../../types/getFeedResponse.interface"
   styleUrls: ['./feed.component.scss']
 })
 
-export class FeedComponent implements OnInit{
+export class FeedComponent implements OnInit, OnDestroy{
   @Input ('apiUrl') apiUrlProps: string;
 
-  feed$: Observable<GetFeedResponseInterface | null>
-  error$: Observable<string | null>
-  isLoading$: Observable<boolean>
+  feed$: Observable<GetFeedResponseInterface | null>;
+  error$: Observable<string | null>;
+  isLoading$: Observable<boolean>;
+  limit = environment.limit;
+  baseUrl: string;
+  queryParamsSubscription: Subscription;
+  currentPage: number;
 
   constructor(
     private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
     this.initializeValues();
-    this.fetchData()
+    this.fetchData();
+    this.initializeListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
   }
 
   initializeValues(): void {
     this.feed$ = this.store.pipe(select(feedSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.baseUrl = this.router.url.split('?')[0];
   }
 
   fetchData(): void {
     this.store.dispatch(getFeedAction({url: this.apiUrlProps}))
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe((params: Params) => {
+      this.currentPage = Number(params.page || '1');
+    })
   }
 }
